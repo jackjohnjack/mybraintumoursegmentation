448cfd17605a3d5d9a17177187dd28fecf63fe22
Li Jie <145555369+jackjohnjack@users.noreply.github.com>
2024-08-21 14:30:33 +0100

    Update Unet3+.py

--- Unet3+.py
+++ Unet3+.py
@@ -549,7 +549,7 @@ for i in range(bias_index + 1,num_epochs+bias_index + 1):
        
     
 print("Training completed")
-model_path="./3ClassNet3-epochs"
+model_path="./UNet3-epochs"
 model_path_pth = model_path + ".pth"
 torch.save(UNET_Model,model_path)
 torch.save({
@@ -557,7 +557,7 @@ torch.save({
     'optimizer_state_dict': optimizer_function.state_dict(),
     }, model_path_pth)
         
-recode_path="./3ClassNet3-result_recode"
+recode_path="./UNet3-result_recode"
 with open(recode_path, 'w') as file:
     for label in result_recode:
         file.write(f"{label}\n")
